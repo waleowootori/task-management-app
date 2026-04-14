450a406b3b27316b9bd54dbb8b126beb98015206
Babawale Owootori <babawaleowootori@gmail.com>
2026-04-14 15:40:16 +0100

    UI upgrade

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://task-management-app-one-self.vercel.app",
+  }),
+);
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
