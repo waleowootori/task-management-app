97aae457ab21242167cb023cf1bd10eeea6ead0a
Babawale Owootori <babawaleowootori@gmail.com>
2026-04-15 23:30:21 +0100

    update

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-management-app-uce7.onrender.com/api",
+  baseURL: "https://task-management-app-uce7.onrender.com/api/tasks",
 });
 
 API.interceptors.request.use((req) => {
